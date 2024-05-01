a6d9ca34bff402de4527db448b9822ff938c6d88
will-lin2021 <2021linw39@gmail.com>
2024-04-30 20:59:06 -0500

    Added more error checks and outputs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import fnmatch
 
 folder_loc = ["animCJK/svgsJa", "animCJK/svgsKana"]
@@ -8,7 +7,7 @@ def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
-        
+
 def find_pat(pattern, path):
     result = []
     for root, dirs, files in os.walk(path):
@@ -19,26 +18,38 @@ def find_pat(pattern, path):
 
 def main():
     print("Kanji SVG Getter")
+
+    if not os.path.isdir("./animCJK"):
+        print("Unable to find animCJK git repo folder, exiting...")
+        return
+
+    if not os.path.isdir("./assets"):
+        print("Couldn't find an `assets` folder, creating an `assets` folder...")
+        os.mkdir("./assets")
+
     print("Type the kanji after the `>` then press `ENTER`")
-    
+
     running = True
     delete = False
     while running:
         user_input = input("> ")
         user_input = user_input.split()
-        
+
+        if len(user_input) == 0:
+            continue
+
         match user_input[0]:
             case 'q':
                 running = False
-                
+
                 if len(user_input) > 1 and user_input[1] == 'd':
                     delete = True
-                    
+
                 break
             case _:
                 for char in user_input[0]: # Loop through each character in input
                     unicode = str(int(ord(char)))
-                    
+
                     for folder in folder_loc: # Search through all folders for character
                         filename = find(unicode + ".svg", folder)
                         if filename:
@@ -47,13 +58,36 @@ def main():
                     if not filename: # Unable to find file with given character
                         print("Unable to find svg for {}".format(char))
                         continue
-                
-                    shutil.copyfile(filename, "./assets/{}.svg".format(char)) # Copy character into assets folder
-    
+
+                    with open(filename, "r") as src, open("./assets/{}.svg".format(char), "w") as dest:
+                        skip = False
+
+                        while True:
+                            line = src.readline()
+
+                            if line == '':
+                                break
+
+                            if line == "svg.acjk path[clip-path] {\n":
+                                skip = True
+                                dest.write("/* ")
+
+                            if line == "svg.acjk path[id] {fill:#ccc;}\n":
+                                line = "svg.acjk path[id] {fill:#000;}"
+
+                            dest.write(line.rstrip('\n'))
+
+                            if skip and line == "}\n":
+                                skip = False
+                                dest.write(" */")
+
+                            dest.write("\n")
+
     if delete:
+        print("Deleting svg's in assets folder")
         for file in find_pat("*.svg", "./assets"): # Remove all svg files
             os.remove(file)
-            
+
     print("Exiting...")
 
 if __name__ == "__main__":
